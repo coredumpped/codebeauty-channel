1d75e400ba3f1e81d63956410e9e94036ee03d50
r0mka <romanliveshere@gmail.com>
2021-01-02 06:20:30 -0800

    Add 5-dynamic-arr.cpp

---
+++ 3-pointers/5-dynamic-arr.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+
+int main() {
+    int size; 
+    int staticArray[5];
+
+    std::cout << "Size: "; 
+    std::cin >> size;
+
+    int* myArray = new int[size];
+    for (int i = 0; i < size; i++) {
+        std::cout << "Enter an element: "; 
+        std::cin >> myArray[i];
+    }
+
+    
+
+    for (int i = 0; i < size; i++) {
+        // one way to access array elements
+        // std::cout << myArray[i] << std::endl;
+        
+        // accessing array elements using pointers
+        std::cout << *(myArray + i) << std::endl;
+    }
+    
+    delete []myArray;
+    myArray = NULL;
+
+    std::cout << std::endl << std::endl << std::endl << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
